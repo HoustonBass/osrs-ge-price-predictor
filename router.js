96b77dd80498090270abc2af2d54d813134a3166
Houston Bass <houston_bass@homedepot.com>
2019-03-18 11:25:18 -0400

    adding item lookup by id

--- router.js
+++ router.js
@@ -2,26 +2,30 @@ const express    = require('express');        // call express
 const router = express.Router();
 const osrs = require('./controllers/osrs')
 
-// osrs.fetchIds()
+osrs.fetchIds()
+
+router.get('/ids', (req, res) => {
+    res.send({'ids':osrs.ids()})
+})
 
 router.get('/ids/:id', (req, res) => {
-    var id = parseInt(req.params.id)
-    if(isNaN(id)) {
-        res.status(400).send({'error':'Not valid integer format'})
-    }
+    var id = validId(req, res)
     osrs.fetchById(id).then(data => {
         if(data == -1) {
             res.status(400).send({'error': 'Not valid item id'})
         }
         res.send(data)
     })
-    // if(req.params.id == undefined || parseInt(req.params.id))
-    // fetchById(req.params.id)
-    // res.send(fetchBy(req.params.id))
 })
 
-router.get('/ids', (req, res) => {
-    res.send({'ids':osrs.ids()})
+router.get('/ids/:id/price', (req, res) => {
+    var id = validId(req, res)
+    osrs.fetchById(id).then(data => {
+        if(data == -1) {
+            res.status(400).send({'error': 'Not valid item id'})
+        }
+        res.send(data.item.current.price)
+    })
 })
 
 router.get('/ids/fetch', (req, res) => {
@@ -31,5 +35,13 @@ router.get('/ids/fetch', (req, res) => {
     // }
     // res.send({"fetching": osrs.fetching(), "lastFetch": osrs.lastFetch(), "ids": osrs.ids()})
 })
+
+function validIf(req, res) {
+    var id = parseInt(req.params.id)
+    if(isNaN(id)) {
+        res.status(400).send({'error':'Not valid integer format'})
+    }
+    return id;
+}
 
 module.exports = router;
